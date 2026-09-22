f635d3f9ee3b7cbf2a5af63a4f28557677d8869c
dgg0012 <dgg0012@edu.curio.nl>
2026-09-22 10:56:33 +0200

    Add LogEntry model and in-memory LogService
    Introduce LogEntry model to represent timestamped log messages and update LogService to store entries in a private List<LogEntry>.

---
+++ Keyboard clicker/Models/LogEntry.cs
@@ -0,0 +1,15 @@
+﻿using Keyboard_clicker.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Keyboard_clicker.Models
+{
+    internal class LogEntry
+    {
+        public DateTime TimeStamp { get; set; }
+
+        public string Message { get; set; }
+
+    }
+}
--- Keyboard clicker/Services/LogService.cs
+++ Keyboard clicker/Services/LogService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Keyboard_clicker.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,17 @@ namespace Keyboard_clicker.Services
 {
     class LogService
     {
+        private readonly List<LogEntry> _entries = new List<LogEntry>();
+
+        public IReadOnlyList<LogEntry> Entries => _entries;
+
+        public void Add(string message)
+        {
+            _entries.Add(new LogEntry
+            {
+                TimeStamp = DateTime.Now,
+                Message = message,
+            });
+        }
     }
 }
